fb151c8b8e7fbf79ec422a6a1a41898a3ace6a7d
Mathew <justin.j.mathew@daimler.com>
2021-01-04 21:41:32 +0530

    Updated

--- basics/ds-algos/LinkedList.py
+++ basics/ds-algos/LinkedList.py
@@ -49,6 +49,22 @@ class LinkedLIst:
             print(actual_node.data)
             actual_node=actual_node.nextNode
 
+    def deleteAtHead(self):
+
+        self.head=self.head.nextNode
+        self.numberOFNodes=self.numberOFNodes-1
+
+    def deleteAtTail(self):
+
+        actual_node = self.head
+
+        while actual_node.nextNode.nextNode is not None:
+            actual_node=actual_node.nextNode
+
+        actual_node.nextNode=None
+        self.numberOFNodes=self.numberOFNodes-1
+
+
 
 linked_list=LinkedLIst()
 linked_list.insert_start(4)
@@ -57,3 +73,12 @@ linked_list.insert_start('A String type')
 linked_list.insert_start(1.232)
 linked_list.insert_end(12.232)
 linked_list.traverse()
+
+print('Deleting at the front')
+linked_list.deleteAtHead()
+linked_list.traverse()
+
+print('Delete at the last position')
+linked_list.deleteAtTail()
+linked_list.traverse()
+
